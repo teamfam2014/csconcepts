07a1ea1ad5570ff0d5ec7312d74e0b4b87582999
teamfam <teamfam2014@gmail.com>
2020-09-02 01:11:53 -0400

    renamed and added new logic

---
+++ csconcepts/src/main/java/com/teamfam/algorithms/AllFactors.java
@@ -0,0 +1,59 @@
+package com.teamfam.algorithms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Find the all possible factors of an Integer based on all integers defined
+ * in a set of candidates.
+ */
+public class AllFactors{
+
+    /**
+     * Gather all possible factors of an Integer based on a set of provided candidate integers. Repetition of a candidate 
+     * can occur to sum to the target.
+     * 
+     * @param target the integer to sum up to
+     * @param candidateSet the set of integers to build the sum off of
+     */
+    public Set<List<Integer>> getFactors(Integer target, Set<Integer> candidateSet){
+        Set<List<Integer>> solutionSet = new HashSet<List<Integer>>();
+        for(Integer candidate : candidateSet){
+            List<Integer> factors = new ArrayList<Integer>();
+            if (candidate == target){
+                factors.add(candidate);
+            }else if(target % candidate == 0){
+                //Candidate is a factor of the target. Get the other factor by dividing target into candidate
+                int factor = target / candidate;
+                for(int index = 0; index < factor; index++){
+                    factors.add(candidate);
+                }
+            }else{
+                //Candidate is not a factor of the target. Get the remainder, and get the multiple
+                int factor = target / candidate;
+                int remainder = target % candidate;
+                if (candidateSet.contains(remainder)){
+                    //ensure that the factor is added and the remainder are added 
+                    for(int index = 0; index < factor; index++){
+                        factors.add(candidate);
+                    }
+                    factors.add(remainder);
+                }//else{
+                /*    //we need to break down the remainder to see if there are elements in the candidate set that add to the remainder                    
+                    Set<Set<Integer>> remainderFactorsSolutionSet = getFactors(remainder,candidateSet);
+                    for(Set<Integer> remainderFactorSet : remainderFactorsSolutionSet){
+                        //ensure that the factors are added each time
+                        for(int index = 0; index < factor; index++){
+                            factors.add(candidate);
+                        }                        
+                        factors.addAll(remainderFactorSet);
+                    }
+                }*/
+            }
+            solutionSet.add(factors);
+        }
+        return solutionSet;
+    }
+}
--- csconcepts/src/test/java/com/teamfam/algorithms/AllFactorsTest.java
+++ csconcepts/src/test/java/com/teamfam/algorithms/AllFactorsTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
@@ -13,7 +14,7 @@ import org.junit.Test;
  * Validate that all scenarios of the Greedy Factors class
  * are covered.
  */
-public class GreedyFactorsTest {
+public class AllFactorsTest {
     
     /**
      * If the candidate set contains the target, then it is included in the
@@ -22,16 +23,12 @@ public class GreedyFactorsTest {
     @Test
     public void targetInCandidate(){
         //ARRANGE
-        GreedyFactors gf = new GreedyFactors();
+        AllFactors gf = new AllFactors();
         Integer target = 5;
         Set<Integer> candidateSet = new HashSet<>();
         candidateSet.add(target);
-        Set<Set<Integer>> expectedSolutionSet = new HashSet<>();
-        Set<Integer> targetSet = new HashSet<>();
-        targetSet.add(target);
-        expectedSolutionSet.add(targetSet);
         //ACT
-        Set<Set<Integer>> solutionSet = gf.getFactors(target, candidateSet);
+        Set<List<Integer>> solutionSet = gf.getFactors(target, candidateSet);
         //ASSERT
         assertFalse(solutionSet.isEmpty());
         assertTrue(solutionSet.size() == 1);
@@ -50,26 +47,54 @@ public class GreedyFactorsTest {
     @Test
     public void candidateContainsFactor(){
         //ARRANGE
-        GreedyFactors gf = new GreedyFactors();
+        AllFactors gf = new AllFactors();
         Integer target = 10;
         Integer targetFactor = 5;
         Set<Integer> candidateSet = new HashSet<>();
         candidateSet.add(targetFactor);
-        Set<Set<Integer>> expectedSolutionSet = new HashSet<>();
-        Set<Integer> targetSet = new HashSet<>();
-        targetSet.add(targetFactor);
-        targetSet.add(targetFactor);
-        expectedSolutionSet.add(targetSet);
         //ACT
-        Set<Set<Integer>> solutionSet = gf.getFactors(target, candidateSet);
+        Set<List<Integer>> solutionSet = gf.getFactors(target, candidateSet);
         //ASSERT
         assertFalse(solutionSet.isEmpty());
         assertTrue(solutionSet.size() == 1);
         solutionSet.forEach(factors ->{
-            assertTrue(factors.size() == 1);
+            assertTrue(factors.size() == 2);
             factors.forEach(factor -> {
                 assertEquals(targetFactor,factor);
             });
         });
-    }    
+    } 
+    
+    /**
+     * If the candidate set contains numbers that are not factors of the target
+     * but still add up to the target together, then return the set of those factors
+     * in the solution set.
+     */
+    @Test
+    public void candidateContainsNonFactors(){
+        //ARRANGE
+        AllFactors gf = new AllFactors();
+        Integer target = 5;
+        Set<Integer> candidateSet = new HashSet<>();
+        candidateSet.add(1);
+        candidateSet.add(2);
+        candidateSet.add(3);
+        candidateSet.add(4);
+        candidateSet.add(5);
+        Set<List<Integer>> expectedSolutionSet = new HashSet<>();
+        expectedSolutionSet.add(List.of(1,1,1,1,1));        
+        expectedSolutionSet.add(List.of(1,4));
+        expectedSolutionSet.add(List.of(1,2,2));
+        expectedSolutionSet.add(List.of(1,1,1,2));
+        expectedSolutionSet.add(List.of(2,2,1));
+        expectedSolutionSet.add(List.of(2,3));
+        expectedSolutionSet.add(List.of(3,2));
+        expectedSolutionSet.add(List.of(3,1,1));
+        expectedSolutionSet.add(List.of(4,1));
+        expectedSolutionSet.add(List.of(5));
+        //ACT
+        Set<List<Integer>> solutionSet = gf.getFactors(target, candidateSet);
+        //ASSERT
+    }
+
 }
